be44cd83e24a52acfd92276f34e428648224c23d
StephenSmithwick <Stephen.Smithwick@gmail.com>
2013-10-28 23:00:03 +1100

    adding delta indicator on dashboard

--- app/presenters/indicator_measure.rb
+++ app/presenters/indicator_measure.rb
@@ -12,16 +12,29 @@ class IndicatorMeasure
     @mean ||= calculate_mean @ratings.map(&:score)
   end
 
+  def previous_mean
+    @previous_mean ||= calculate_mean @previous_ratings.map(&:score)
+  end
+
+  def delta
+    if(mean && previous_mean)
+      return "fa-level-down" if previous_mean > mean
+      return "fa-level-up" if previous_mean < mean
+    end
+  end
+
   def health
     @health ||= calculate_health(mean)
   end
 
   def self.for_project(project)
-    project_indicators = project.project_indicators
-    project_indicators.map do |project_indicator|
+    current_span = project.spans.current
+    previous_span = project.spans.previous
+    project.project_indicators.map do |project_indicator|
       new( 
         name: project_indicator.indicator.name,
-        ratings: project_indicator.indicator.ratings.on_project(project) )
+        ratings: project_indicator.indicator.ratings.on_span(current_span),
+        previous_ratings: project_indicator.indicator.ratings.on_span(previous_span) )
     end
   end
 
