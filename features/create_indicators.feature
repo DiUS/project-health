c5b87af3f15f4979da8055b35c9915c03c79f693
Tristan Morgan <tmorgan@dius.com.au>
2014-01-30 10:19:35 +1100

    fixes label association to select box, and adds test for indicator creation.

--- features/create_indicators.feature
+++ features/create_indicators.feature
@@ -8,3 +8,13 @@ Feature: Create  indicators
     Then I fill in "Name" with "Testing"
     And I press "Create Category"
     Then I should see "Name: Testing"
+
+  Scenario: user creates an indicator
+    Given that "Technical" exists in categories
+    And user opens the new indicators page
+    Then I fill in "Name" with "Test Indicator"
+    And I select the "Category" value "Technical"
+    And I fill in "Sort order" with "13"
+    And I press "Create Indicator"
+    Then I should see "Name: Test Indicator"
+    And I should see "Category: Technical"
--- features/step_definitions/generic_steps.rb
+++ features/step_definitions/generic_steps.rb
@@ -6,12 +6,20 @@ Given(/^user opens the (\w*) page$/) do |page_name|
   visit '/' + page_name
 end
 
+Given(/^that "(.*?)" exists in categories/) do |name|
+  create_category name
+end
+
 Then(/^(.*) is displayed$/) do |label|
   expect(page).to have_content(label)
 end
 
 Then(/^I fill in "(.*?)" with "(.*?)"$/) do |field, value|
   fill_in field, with:value
+end
+
+Then(/^I select the "Category" value "(.*?)"$/) do |value|
+  select value, :from => "Category"
 end
 
 Then(/^I press "(.*?)"$/) do |button|
@@ -29,4 +37,10 @@ def find_a_button name
   end
 
   return find_link(name)
+end
+
+def create_category name
+  visit '/categories/new'
+  fill_in 'Name', with:name
+  find_a_button('Create Category').click
 end
